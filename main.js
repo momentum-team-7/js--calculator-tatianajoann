1d8c2bcfdad137585bb5cf45bb4ed71ad3e98875
tatiana perez <tjperez@ncsu.edu>
2021-01-27 18:39:42 -0500

    calculator buttons work but don't do math

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let buttons = document.querySelectorAll("button")
 
 for (let button of buttons) {
   button.addEventListener('click',event => {
-      const {target} = event;
+      const target = event.target;
     if (!target.matches("button")) {
         return;
     }
@@ -12,15 +12,22 @@ for (let button of buttons) {
         return;
     }
     if (target.classList.contains("decimal")) {
-        console.log("decimal", target.value);
+        inputDecimal(target.value);
+        updateDisplay();
         return;
     }
     if (target.classList.contains("clear")) {
         console.log("clear", target.value);
         return;
     }
+
+    inputDigit(target.value);
+    updateDisplay();
   })
 }
+
+
+
 
 const calculator = {
     displayValue: "0",
@@ -29,12 +36,26 @@ const calculator = {
     operator: null
 };
 
+function inputDigit(digit) {
+    let {displayValue} = calculator;
+    if (displayValue === '0') {
+        calculator.displayValue = digit
+    } else {
+        calculator.displayValue = displayValue + digit
+    }
+}
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue = calculator.displayValue + dot;
+    }
+}
+
+
 function updateDisplay () {
     const display = document.querySelector(".calcdisplay");
     display.value = calculator.displayValue;
 }
-updateDisplay();
-function dosomething() {
 
-}
+
 
